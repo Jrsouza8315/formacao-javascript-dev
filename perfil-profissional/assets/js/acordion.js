7713b48d20312883dfeb9b522775421f8eeb8bbe
Jrsouza8315 <hbrcomercialssa@gmail.com>
2022-09-24 23:38:45 -0300

    add

--- perfil-profissional/assets/js/acordion.js
+++ perfil-profissional/assets/js/acordion.js
@@ -1,1 +1,14 @@
-docu
+const acordionTriggers = document.querySelectorAll('.acordion .trigger')
+
+acordionTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordion = trigger.parentElement
+        const isOpen = acordion.classList.contains('open')
+
+        if (isOpen) {
+            acordion.classList.remove('open') //= Array.of(acordion.classList).filter(className => className !== 'open')
+        } else {
+            acordion.classList.add('open')
+        }
+     })
+})
